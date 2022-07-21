ebc1bc6f650ab3fe99a5c84adfa90a34ff1cbf38
rizkiramadhanx <rizkijitu77@gmail.com>
2022-07-22 05:16:00 +0700

    hmmm

--- src/context/index.js
+++ src/context/index.js
@@ -1,5 +1,29 @@
-import { createContext } from "react";
+// import { createContext, useState } from "react";
 
-const ContextGlobal = createContext();
+// const ContextGlobal = createContext();
 
-export default ContextGlobal;
+// const ContextGlobalProvider = (props) => {
+//   const { state, setState } = useState({
+//     name: null,
+//     isLogin: false,
+//     time: false,
+//   });
+
+//   const change = (name, isLogin, time) => {
+//     setState(...state, { name, isLogin, time });
+//   };
+
+//   return (
+//     <ContextGlobal.Provider value={{ state, change }}>
+//       {props.children}
+//     </ContextGlobal.Provider>
+//   );
+// };
+
+// export default ;
+
+const initialState = {
+  name: null,
+  isLogin: false,
+  time: false,
+};
